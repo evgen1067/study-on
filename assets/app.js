9eb6e6175608097a7cb0b92a6526078da75af9ba
evgen1067 <bogomolov.evgen1067@mail.ru>
2022-10-03 00:47:50 +0300

    подправил все шаблоны под Bootstrap 5

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,12 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
+// import @popperjs/core
+import '@popperjs/core'
+
+// import JQuery
+import 'jquery/dist/jquery.min';
+
 // import Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
