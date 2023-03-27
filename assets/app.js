311ec55a60b296342884c4437a2b964179847413
Евгений Богомолов <bogomolov.evgen1067@mail.ru>
2023-03-27 11:33:09 +0300

    Lesson 3

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// start the Stimulus application
+import './bootstrap';
+
+// import @popperjs/core
+import '@popperjs/core'
+
+// import JQuery
+import 'jquery/dist/jquery.min';
+
+// import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+import './styles/app.scss';
